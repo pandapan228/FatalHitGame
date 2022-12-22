9c34a29e788db129783e97d6bca94be201ecaf6d
pandapan228 <missis.gonka@yandex.ru>
2022-12-22 14:22:26 +0300

    скелет программы

---
+++ main.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+pygame.init()
+width, height = 2978, 1675
+screen = pygame.display.set_mode((width, height))
+
+
+class Greeting:
+    pass
+
+
+running = True
+while running:
+    pass
+
+
+# изображение находится в папке дата, размеры экрана сделаны под размер изображения,
+# изображение должно быть на весь экран
