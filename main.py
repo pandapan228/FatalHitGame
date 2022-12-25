72c8020af3edebe44c90d81013d9cc8486a1324e
pandapan228 <missis.gonka@yandex.ru>
2022-12-25 22:01:50 +0300

    Рабочий начальный экран (Андреев)

--- main.py
+++ main.py
@@ -1,19 +1,51 @@
 import pygame
+import os
+import sys
 
 
 pygame.init()
-width, height = 2978, 1675
-screen = pygame.display.set_mode((width, height))
+WIDTH, HEIGHT = 1500, 800
+FPS = 50
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+clock = pygame.time.Clock()
 
 
-class Greeting:
-    pass
+def terminate():
+    pygame.quit()
+    sys.exit()
 
 
-running = True
-while running:
-    pass
+def load_image(name, colorkey=None):
+    if not os.path.isfile(name):
+        print('Файл не найден')
+        sys.exit()
+    image = pygame.image.load(name)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image.convert_alpha()
+    return image
 
 
-# изображение находится в папке дата, размеры экрана сделаны под размер изображения,
-# изображение должно быть на весь экран
+def greeting():
+    fon0 = load_image('data/dungeon_greeting.png')
+    fon = pygame.transform.scale(fon0, (1500, 800))
+    screen.blit(fon, (0, 0))
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+
+greeting()
