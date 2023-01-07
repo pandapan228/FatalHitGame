0f384f9aa1a73e0b89384340fef35f9ddbe572f9
pandapan228 <missis.gonka@yandex.ru>
2023-01-07 11:52:06 +0300

    Первый уровень без игрока и мобов (Андреев)

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pygame
-import os
 import sys
-
+import os
 
 pygame.init()
-WIDTH, HEIGHT = 1500, 800
+WIDTH, HEIGHT = 1000, 700
 FPS = 50
-
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 
@@ -31,11 +29,14 @@ def load_image(name, colorkey=None):
     return image
 
 
-def greeting():
-    fon0 = load_image('data/dungeon_greeting.png')
-    fon = pygame.transform.scale(fon0, (1500, 800))
+def make_fon(name):
+    fon0 = load_image(name)
+    fon = pygame.transform.scale(fon0, (1000, 700))
     screen.blit(fon, (0, 0))
 
+
+def greeting():
+    make_fon('data/dungeon_greeting.png')
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -47,5 +48,105 @@ def greeting():
         clock.tick(FPS)
 
 
-
 greeting()
+
+
+def load_level(filename):
+    # читаем уровень, убирая символы перевода строки
+    with open(filename, 'r') as mapFile:
+        level_map = [line.strip() for line in mapFile]
+
+    # и подсчитываем максимальную длину
+    max_width = max(map(len, level_map))
+
+    # дополняем каждую строку пустыми клетками ('.')
+    return list(map(lambda x: list(x.ljust(max_width, '.')), level_map))
+
+
+tile_images = {
+    'wall': load_image('data/wall2.png'),
+}
+player_image = None
+
+tile_width = tile_height = 75
+
+
+class Tile(pygame.sprite.Sprite):
+    def __init__(self, tile_type, pos_x, pos_y):
+        super().__init__(tiles_group, all_sprites)
+        self.image = tile_images[tile_type]
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x, tile_height * pos_y)
+
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__(player_group, all_sprites)
+        self.image = player_image
+        self.rect = self.image.get_rect().move(
+            tile_width * pos_x + 15, tile_height * pos_y + 5)
+        self.pos = pos_x, pos_y
+
+
+all_sprites = pygame.sprite.Group()
+tiles_group = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+
+
+def generate_level(level):
+    new_player, x, y = None, None, None
+    for y in range(len(level)):
+        for x in range(len(level[y])):
+            if level[y][x] == '#':
+                Tile('wall', x, y)
+            elif level[y][x] == '@':
+                new_player = Player(x, y)
+                level[y][x] = '.'
+    # вернем игрока, а также размер поля в клетках
+    return new_player, x, y
+
+
+level_map = load_level('data/map.txt')
+player, level_x, level_y = generate_level(level_map)
+
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            terminate()
+        if event.type == pygame.KEYDOWN:
+            x, y = player.pos
+            if event.key == pygame.K_LEFT:
+                if x > 0 and level_map[y][x - 1] == '.':
+                    player.pos = x - 1, y
+                    player.rect = player.image.get_rect().move(tile_width * player.pos[0] + 15,
+                                                               tile_height * player.pos[1] + 5)
+            elif event.key == pygame.K_RIGHT:
+                if x < level_x and level_map[y][x + 1] == '.':
+                    player.pos = x + 1, y
+                    player.rect = player.image.get_rect().move(tile_width * player.pos[0] + 15,
+                                                               tile_height * player.pos[1] + 5)
+            elif event.key == pygame.K_UP:
+                if y > 0 and level_map[y - 1][x] == '.':
+                    player.pos = x, y - 1
+                    player.rect = player.image.get_rect().move(tile_width * player.pos[0] + 15,
+                                                               tile_height * player.pos[1] + 5)
+            elif event.key == pygame.K_DOWN:
+                if y < level_y and level_map[y + 1][x] == '.':
+                    player.pos = x, y + 1
+                    player.rect = player.image.get_rect().move(tile_width * player.pos[0] + 15,
+                                                               tile_height * player.pos[1] + 5)
+    make_fon('data/phone1.jpg')
+    tiles_group.draw(screen)
+    pygame.display.flip()
+    clock.tick(FPS)
+
+
+end_screen()
+terminate()
